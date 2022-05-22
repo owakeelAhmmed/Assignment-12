da3548e027436b3f13909f5abc1afc6f1a972577
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-05-23 04:40:53 +0600

    Headder section done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Headder from './Headder';
 
 
 const Home = () => {
     return (
         <div className='bg-primary h-screen'>
-            
+            <Headder/>
         </div>
     );
 };
