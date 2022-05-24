37ec2502e623b33ab36384ebd94cd6999b3c3fd7
owakeelAhmmed <mdowakeelahmed5678@gmail.com>
2022-05-24 14:00:23 +0600

    User navigate complete

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,5 +1,8 @@
+import { signOut } from "firebase/auth";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 
 
@@ -7,13 +10,18 @@ import { Link } from "react-router-dom";
 
 
 function Navbar() {
+  const [user, loading, error] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+  };
 
   const menuItems = <>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/login'>Login</Link></li>
+            <li>{user ? <button onClick={logout} class="btn btn-outline btn-secondary">SignOut</button> : <Link to='/login'>Login</Link>}</li>
           
          </>
 
